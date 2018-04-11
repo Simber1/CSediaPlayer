8b51587c7f18a8a6fac20ee62290d1c888f75974
Peter <simber2@btinternet.com>
2018-04-11 21:53:41 +0100

    Everything but Seeking works

--- CSediaPlayer/MainWindow.xaml.cs
+++ CSediaPlayer/MainWindow.xaml.cs
@@ -1,71 +1,163 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
 using Microsoft.Win32;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	/// 
 
-    public partial class MainWindow : Window
-    {
+	public partial class MainWindow : Window
+	{
 
-        string path;
-        WMPLib.WindowsMediaPlayer player;
+		bool muted;
+		double volume;
+		double prevVolume;
+		string path;
+		bool isChecked = false;
+		bool firstRun = true;
 
-        public MainWindow()
-        {
-            player = new WMPLib.WindowsMediaPlayer();
+		WMPLib.WindowsMediaPlayer player;
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            player.settings.volume = 25;
-            player.settings.autoStart = true;
+			muted = false;
 
-            InitializeComponent();
+			player = new WMPLib.WindowsMediaPlayer();
 
-            SongSelect();
-        }
+			volume = 25;
+			player.settings.volume = Convert.ToInt32(volume);
+			player.settings.autoStart = true;
+			VolumeUpdate();
+			SongSelect();
 
-        private void SongSelect()
-        {
-            
-            path = null;
-            OpenFileDialog file = new OpenFileDialog();
-            file.Filter = "Music (*.mp3)|*.mp3;*.m4a;*.wav|All Files (*.*)|*.*";
-            file.FilterIndex = 1;
-            file.Multiselect = false;
-
-            if (file.ShowDialog() == true)
-            {
-                path = file.FileName;
-            }
-            Console.WriteLine(path);
-            player.URL = path;
-        }
-
-        private void Pause(object sender, RoutedEventArgs e)
-        {
-            player.controls.pause();
-        }
-
-        private void Play(object sender, RoutedEventArgs e)
-        {
-            player.controls.play();
-        }
-
-        private void Stop(object sender, RoutedEventArgs e)
-        {
-            player.controls.stop();
-        }
-
-        private void Select(object sender, RoutedEventArgs e)
-        {
-            SongSelect();
-        }
-
-    }
+			player.PositionChange += UpdateSongPosition;
 
 
 
+
+		}
+
+		private void SongSelect()
+		{
+
+			path = null;
+			OpenFileDialog file = new OpenFileDialog();
+			file.Filter = "Music (*.mp3)|*.mp3;*.m4a;*.wav|All Files (*.*)|*.*";
+			file.FilterIndex = 1;
+			file.Multiselect = false;
+
+			if (file.ShowDialog() == true)
+			{
+				path = file.FileName;
+			}
+			Console.WriteLine(path);
+			player.URL = path;
+			Current_Playing.Text = player.currentMedia.name;
+			Progress_Slider.Maximum = Convert.ToInt32(player.currentMedia.duration);
+			Console.WriteLine(player.currentMedia.durationString);
+			Max_Time.Text = player.currentMedia.durationString;
+
+
+
+		}
+
+		
+		private void UpdateSongPosition(double a, double b)
+		{
+
+			Progress_Slider.Value = player.controls.currentPosition * 100 / player.currentMedia.duration;
+			Progress_Slider.Value = Progress_Slider.Value;
+			Current_Time.Text = player.controls.currentPositionString;
+			Progress_Slider.Maximum = player.currentMedia.duration;
+			Max_Time.Text = player.currentMedia.durationString;
+		}
+
+
+		private void VolumeUpdate()
+		{
+			player.settings.volume = Convert.ToInt32(volume); //Updating the players volume 
+			Volume_Slider.Value = volume;  //Setting the sliders pos = to the volume
+		}
+
+		private void Pause(object sender, RoutedEventArgs e)
+		{
+			player.controls.pause();
+		}
+
+		private void Play(object sender, RoutedEventArgs e)
+		{
+			player.controls.play();
+		}
+
+		private void Stop(object sender, RoutedEventArgs e)
+		{
+			player.controls.stop();
+		}
+
+		private void Select(object sender, RoutedEventArgs e)
+		{
+			SongSelect();
+		}
+
+		private void Mute()
+		{
+			if (Mute_Toggle.IsChecked == true)
+			{
+				prevVolume = volume;  //muting
+				volume = 0;
+				Mute_Toggle.Content = "Unmute";
+			}
+			else
+			{
+				volume = prevVolume; //unmuting
+				Mute_Toggle.Content = "Mute";
+			}
+
+			VolumeUpdate();
+		}
+
+
+		private void MuteChecked(object sender, RoutedEventArgs e)
+		{
+			Mute();
+
+			Console.WriteLine("Mute Checked");
+		}
+
+		private void MuteUnchecked(object sender, RoutedEventArgs e)
+		{
+			Mute();
+
+			Console.WriteLine("Mute Unchecked");
+		}
+
+		private void VolumeSlider(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{ 
+			volume = Volume_Slider.Value;
+			VolumeUpdate();
+		}
+
+		public static void DelayAction(int millisecond, Action action)
+		{
+			var timer = new DispatcherTimer();
+			timer.Tick += delegate
+
+			{
+				action.Invoke();
+				timer.Stop();
+			};
+
+			timer.Interval = TimeSpan.FromMilliseconds(millisecond);
+			timer.Start();
+		}
+
+	}
 }
+
+
